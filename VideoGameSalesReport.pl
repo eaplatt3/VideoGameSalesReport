33c309addc6c7869e521685a64d12accbaf7abfe
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-05-12 19:49:01 -0400

    Initial commit

---
+++ VideoGameSalesReport.pl
@@ -0,0 +1,16 @@
+#########################################
+# Earl Platt III
+# 5/12/2019
+# BCS 316 - Perl Programming
+# Video Game Sales Analysis 
+# Input - Data Pulled in from CSV
+# Output - Displays Menu & User can choose
+#          What Data They Want to See
+##########################################
+
+use strict;
+use warnings;
+use Term::Menu;
+use ANSI::Color;
+
+
