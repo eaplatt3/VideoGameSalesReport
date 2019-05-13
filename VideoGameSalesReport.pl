e1c5bbf02ea9711537ef1fe78e4975b5120eb5f8
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-05-12 21:06:36 -0400

    pulled in file

--- VideoGameSalesReport.pl
+++ VideoGameSalesReport.pl
@@ -1,4 +1,4 @@
-#########################################
+##########################################
 # Earl Platt III
 # 5/12/2019
 # BCS 316 - Perl Programming
@@ -6,11 +6,27 @@
 # Input - Data Pulled in from CSV
 # Output - Displays Menu & User can choose
 #          What Data They Want to See
-##########################################
+###########################################
 
 use strict;
 use warnings;
-use Term::Menu;
-use ANSI::Color;
+#use Term::Menu;
+#use ANSI::Color;
+
+my $file = $ARGV[0] or die "Need to open CSV file in command line\n";
+my @fields;
+my $fields;
+
+
+open(my $vgsales, '<', $file) or die "Unable to open CSV file.\n";
+
+while (my $line = <$vgsales>){
+	chomp $line;
+	
+	@fields = split "'", $line;
+	
+	#print "@fields\n";
+	
+}
 
 
